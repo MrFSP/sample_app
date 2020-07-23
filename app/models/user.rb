bc52f8d1855d1bc6256a07ceb1e9ef56ae663f15
Pavel Egorov <epe.m@mail.ru>
2020-07-23 12:07:05 +0300

    Secure passord added.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
                     length: { maximum: 255 },
                     format: { with: URI::MailTo::EMAIL_REGEXP },
                     uniqueness: true
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
