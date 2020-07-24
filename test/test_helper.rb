7e0066a6f97597ad4ee013d7cb6e86e63faa29ae
Pavel Egorov <epe.m@mail.ru>
2020-07-24 21:45:13 +0300

    Test coverage with simplecov added.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,12 @@
+require 'simplecov'
+SimpleCov.start 'rails'
+Rails.application.eager_load!
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 require "minitest/reporters"
+
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
